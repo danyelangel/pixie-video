d65116ffccc2168f93a5eedc384e4a3f32396672
webcatio <danielangel@Daniels-MacBook-Pro.local>
2016-10-13 03:15:08 -0500

    plop

--- dist/app.js
+++ dist/app.js
@@ -1,25 +1,27 @@
+'use strict';
+
 var midi = require('midi');
 var debounce = require('debounce');
 var Omx = require('node-omxplayer');
- 
+
 // Set up a new input. 
 var input = new midi.input();
- 
+
 function runVideo(channel) {
-  var player = Omx(`clip${channel}.mp4`);
+  var player = Omx('clip' + channel + '.mp4');
 }
 
 function debouncedHandler(deltaTime, message) {
-  return debounce((deltaTime, message) => {
-    if ((message[0]-128)/16 > 0) {
-      runVideo((59 - message[1]));
+  return debounce(function (deltaTime, message) {
+    if ((message[0] - 128) / 16 > 0) {
+      runVideo(59 - message[1]);
     }
   }, 100);
 }
 
 // Configure a callback. 
 input.on('message', debouncedHandler());
- 
+
 // Open the first available input port. 
 input.openPort(0);
 
